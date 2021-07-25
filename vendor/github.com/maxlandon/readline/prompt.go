a9f240e656ebbdaf3e6b71c1bee6bac0c0666610
maxlandon <maximelandon@gmail.com>
2021-07-25 13:04:37 -0400

    Update readline lib with prompt construction logic fixes

--- vendor/github.com/maxlandon/readline/prompt.go
+++ vendor/github.com/maxlandon/readline/prompt.go
@@ -210,11 +210,13 @@ func (rl *Instance) computePromptVim() {
 			// Vim status might be empty, but we don't care
 			rl.realPrompt = append(rl.realPrompt, vimStatus...)
 		}
-		if rl.MultilinePrompt != "" {
-			rl.realPrompt = append(rl.realPrompt, []rune(rl.MultilinePrompt)...)
-		} else {
-			rl.realPrompt = append(rl.realPrompt, rl.defaultPrompt...)
-		}
+		// We add the multiline prompt anyway, because it might be empty and thus have
+		// no effect on our user interface, or be specified and thus needed.
+		// if rl.MultilinePrompt != "" {
+		rl.realPrompt = append(rl.realPrompt, []rune(rl.MultilinePrompt)...)
+		// } else {
+		//         rl.realPrompt = append(rl.realPrompt, rl.defaultPrompt...)
+		// }
 	}
 
 	// Strip color escapes
@@ -233,11 +235,13 @@ func (rl *Instance) computePromptEmacs() {
 		if rl.mainPrompt != "" {
 			rl.realPrompt = []rune(rl.mainPrompt)
 		}
-		if rl.MultilinePrompt != "" {
-			rl.realPrompt = append(rl.realPrompt, []rune(rl.MultilinePrompt)...)
-		} else {
-			rl.realPrompt = append(rl.realPrompt, rl.defaultPrompt...)
-		}
+		// We add the multiline prompt anyway, because it might be empty and thus have
+		// no effect on our user interface, or be specified and thus needed.
+		// if rl.MultilinePrompt != "" {
+		rl.realPrompt = append(rl.realPrompt, []rune(rl.MultilinePrompt)...)
+		// } else {
+		//         rl.realPrompt = append(rl.realPrompt, rl.defaultPrompt...)
+		// }
 	}
 
 	// Strip color escapes
