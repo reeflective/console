40f9186a5b8c6b6a8f6d1afc46a69104e2b4139f
Alex Jeannopoulos <alex@jeannopoulos.com>
2025-03-01 10:48:49 -0500

    latest new executor

--- console.go
+++ console.go
@@ -1,7 +1,11 @@
 package console
 
 import (
+	"bytes"
 	"fmt"
+	"github.com/alexj212/console/parser"
+	"github.com/spf13/cobra"
+	"os"
 	"strings"
 	"sync"
 
@@ -238,4 +242,86 @@ func (c *Console) activeMenu() *Menu {
 
 	// Else return the default menu.
 	return c.menus[""]
+}
+
+func (c *Console) executeLine(menu *Menu, line string) {
+	if line == "" {
+		return
+	}
+	outFile, commands, err := parser.ParseCommands(line)
+	if err != nil {
+		fmt.Printf("executeLine parsing line `%s` error: %s\n", line, err.Error())
+		return
+	}
+	if len(commands) == 0 {
+		return
+	}
+
+	rootCmd := menu.Root()
+
+	var outputBuffer bytes.Buffer
+	for _, command := range commands {
+		// Run user-provided pre-run line hooks,
+		// which may modify the input line args.
+		args := append([]string{command.Cmd}, command.Args...)
+		args, err = c.runLineHooks(args)
+		if err != nil {
+			fmt.Printf("executeLine runLineHooks error: %s\n", err.Error())
+			continue
+		}
+
+		menu.Command = rootCmd
+
+		// Run all pre-run hooks and the command itself
+		// Don't check the error: if its a cobra error,
+		// the library user is responsible for setting
+		// the cobra behavior.
+		// If it's an interrupt, we take care of it.
+		output, err := c.executeSingleCommand(menu, rootCmd, command, args)
+		if err != nil {
+			fmt.Printf("executeLine %v\n", err)
+			break
+		}
+		outputBuffer.WriteString(output)
+	}
+
+	// Print the output of the last command
+	output := outputBuffer.String()
+	lines := strings.Split(output, "\n")
+	for _, line := range lines {
+		fmt.Printf("%s\n", string(line))
+	}
+
+	// Handle output redirection if specified
+	if outFile != "" {
+		if err := os.WriteFile(outFile, outputBuffer.Bytes(), 0644); err != nil {
+			fmt.Printf("executeLine failed to write to file `%v` error: %v", outFile, err)
+		}
+	}
+
+}
+
+func (c *Console) executeSingleCommand(menu *Menu, rootCmd *cobra.Command, cmd *parser.ExecCmd, line []string) (string, error) {
+	out := &bytes.Buffer{}
+	var in *bytes.Buffer
+
+	if err := c.execute(menu, rootCmd, line, in, out, false); err != nil {
+		return out.String(), fmt.Errorf("failed to execute: `%s` error: %w", line, err)
+	}
+
+	// If the command is part of a pipe, use the input from the previous command
+	if cmd.Pipe != nil {
+		//fmt.Printf("pipe command: %v\n", cmd.Pipe)
+		filtered := &bytes.Buffer{}
+		line := append([]string{cmd.Pipe.Cmd}, cmd.Pipe.Args...)
+		//fmt.Printf("line: %v\n", line)
+		if err := c.execute(menu, rootCmd, line, out, filtered, false); err != nil {
+			//fmt.Printf("failed to execute: `%s` error: %w", line, err)
+			return out.String(), fmt.Errorf("failed to execute: `%s` error: %w", line, err)
+		}
+		//fmt.Printf("filtered: %v\n", filtered.String())
+		return filtered.String(), nil
+	}
+
+	return out.String(), nil
 }
